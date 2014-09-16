eb3df2612b2e7b8d0445cec8e78cdd6d4ab76bf1
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-09-16 23:00:24 +0400

    Update Application_Start(), Register NinjectControllerFactory in MVC Framework

--- InternetStore.WebUI/Global.asax.cs
+++ InternetStore.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InternetStore.WebUI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,8 @@ namespace InternetStore.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
