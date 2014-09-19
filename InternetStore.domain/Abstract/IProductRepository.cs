033eddf1e0e71f90fabf62bbb4cbdc94871c30a2
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-09-19 21:39:25 +0400

    Add Abstract Repository - Interface IProductRepository

---
+++ InternetStore.domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternetStore.domain.Entities;
+
+namespace InternetStore.domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
