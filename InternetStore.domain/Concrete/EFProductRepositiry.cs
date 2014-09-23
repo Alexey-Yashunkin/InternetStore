9b7c92fb5e9562a374880b6a95da52b2d5dfc68a
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-09-24 00:27:52 +0400

    Add EFProductRepository

---
+++ InternetStore.domain/Concrete/EFProductRepositiry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternetStore.Domain.Abstract;
+using InternetStore.Domain.Entities;
+
+namespace InternetStore.Domain.Concrete
+{
+    public class EFProductRepositiry : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
