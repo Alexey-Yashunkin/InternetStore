9726463d8117cdf96175cb5c7c4a1ae581149020
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-10-13 23:16:38 +0400

    Add class CartModelBinder

---
+++ InternetStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternetStore.Domain.Entities;
+
+namespace InternetStore.WebUI.Binders
+{
+    public class CartModelBinder: IModelBinder {
+        private const string sessionKey = "Cart";
+        public object BindModel (ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // Получаем объект Cart из сеанса
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            
+            // Создаем объект Cart, если его не обнаружено в данных сеанса
+            if (cart == null) {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            return cart;
+        }
+    }
+}
