c88e1a0ed356a10224adc285b3196fd2288c6874
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-10-13 23:20:54 +0400

    Register CartModelBilder

--- InternetStore.WebUI/Global.asax.cs
+++ InternetStore.WebUI/Global.asax.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using InternetStore.Domain.Entities;
+using InternetStore.WebUI.Binders;
 
 namespace InternetStore.WebUI
 {
@@ -25,6 +27,7 @@ namespace InternetStore.WebUI
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
