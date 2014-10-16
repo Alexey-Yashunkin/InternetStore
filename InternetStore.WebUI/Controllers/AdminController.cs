5dbbd580f2fee296972b25c8b42f862c67520231
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-10-16 18:20:14 +0400

    Add admin controller

---
+++ InternetStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternetStore.Domain.Abstract;
+
+namespace InternetStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+    }
+}
