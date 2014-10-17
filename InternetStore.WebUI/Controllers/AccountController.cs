de263d2b0c1023ebb7e89a4ca85ce781fdedc2ca
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-10-17 22:51:34 +0400

    Add account controller

---
+++ InternetStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternetStore.WebUI.Infrastructure.Abstract;
+using InternetStore.WebUI.Models;
+
+namespace InternetStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthProvider authProvider;
+
+        public AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Uncorrect username or password");
+                    return View();
+                }
+            }
+            return View();
+        }
+
+    }
+}
