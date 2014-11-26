5f710b73f06f74e2c643471be535d216811f30f4
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-11-26 23:11:00 +0300

    Add log4net library

--- InternetStore.WebUI/Controllers/CartController.cs
+++ InternetStore.WebUI/Controllers/CartController.cs
@@ -6,11 +6,14 @@ using System.Web.Mvc;
 using InternetStore.Domain.Abstract;
 using InternetStore.Domain.Entities;
 using InternetStore.WebUI.Models;
+using log4net;
+using log4net.Config;
 
 namespace InternetStore.WebUI.Controllers
 {
     public class CartController : Controller
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(CartController));
         private IProductRepository repository;
         public CartController(IProductRepository repo)
         {
@@ -29,23 +32,27 @@ namespace InternetStore.WebUI.Controllers
 
         public RedirectToRouteResult AddToCart(Cart cart, int productID, string returnUrl)
         {
+            log4net.Config.XmlConfigurator.Configure();
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productID);
             if (product != null)
             {
                 cart.AddItem(product, 1);
             }
+            log.Info("Product add to cart");
             return RedirectToAction("Index", new { returnUrl });
         }
 
         public RedirectToRouteResult RemoveFromCart(Cart cart, int productID, string returnUrl)
         {
+            log4net.Config.XmlConfigurator.Configure();
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productID);
             if (product != null)
             {
                 cart.RemoveLine(product);
             }
+            log.Info("Product delete from cart");
             return RedirectToAction("Index", new { returnUrl });
         }
 
