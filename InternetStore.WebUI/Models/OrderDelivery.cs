a3b9df8935ae1e31358f87f73bb270e06c8b4a7d
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-12-20 14:33:29 +0300

    Add delivery and Payment view models

---
+++ InternetStore.WebUI/Models/OrderDelivery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InternetStore.WebUI.Models
+{
+    [Serializable]
+    public class OrderDelivery
+    {
+        public string Delivery { get; set; }
+        public string[] GetDelivery()
+        {
+            return new[] { "Oneself", "InHome" };
+        }
+    }
+}
---
+++ InternetStore.WebUI/Models/OrderPayment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InternetStore.WebUI.Models
+{
+    [Serializable]
+    public class OrderPayment
+    {
+        public string Payment { get; set; }
+        public string[] GetPeyment()
+        {
+            return new[] { "PayCard", "PayMoney" };
+        }
+    }
+}
